29d2425576070ef190fdb3c66d5881094bea5f8b
j7raversion <f7th1m@gmail.com>
2025-12-06 21:57:22 +0800

    last pratikum

--- pwd_2441919024/includes/header.php
+++ pwd_2441919024/includes/header.php
@@ -1,12 +1,13 @@
 <body>
-<header>
-    <h1>praktikum CSS Gabungan</h1>
-</header>
-<nav>
-    <ul>
-        <li><a href= "halaman1.html">Halaman 1</a></li>
-        <li><a href= "halaman2.html">Halaman 2</a></li>
-        <li><a href= "halaman3.html">Halaman 3</a></li>
-        <li><a href= "halaman4.html">Halaman 4</a></li>
-    </ul>
-</nav>
+    <header>
+        <h1>Praktikum CSS Gabungan</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="berita.php">Berita</a></li>
+            <li><a href="halaman3.html">Halaman 3</a></li>
+            <li><a href="halaman4.html">Halaman 4</a></li>
+        </ul>
+    </nav>
